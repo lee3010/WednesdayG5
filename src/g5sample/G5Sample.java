c0dc820f0953350805c75763015a7e49a0e075bf
Tarc <Tarc@PC177>
2018-10-31 15:02:08 +0800

    add product first changes first version

--- src/g5sample/G5Sample.java
+++ src/g5sample/G5Sample.java
@@ -15,7 +15,9 @@ public class G5Sample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hello Sohai");
+       System.out.println("Hello Sohai2");
+       
     }
     
 }
